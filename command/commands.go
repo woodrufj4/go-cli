1b960cbe9228f3c3614716342858d979f47b2d31
James Woodruff <james.woodruff4@gmail.com>
2018-12-09 14:25:29 -0500

    Removed ui colors (#2).

--- command/commands.go
+++ command/commands.go
@@ -1,24 +1,10 @@
 package command
 
 import (
-	"os"
-
 	"github.com/mitchellh/cli"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 func Commands(commandUI cli.Ui) map[string]cli.CommandFactory {
-
-	if terminal.IsTerminal(int(os.Stdout.Fd())) {
-
-		commandUI = &cli.ColoredUi{
-			OutputColor: cli.UiColorNone,
-			InfoColor:   cli.UiColorBlue,
-			ErrorColor:  cli.UiColorRed,
-			WarnColor:   cli.UiColorYellow,
-			Ui:          commandUI,
-		}
-	}
 
 	all := map[string]cli.CommandFactory{
 		"generate": func() (cli.Command, error) {
