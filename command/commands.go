4ec6dec10373b99034cb9aa9e60e570b74cb78d3
James Woodruff <james.woodruff4@gmail.com>
2018-12-09 13:16:28 -0500

    Fixed color issue for windows (#2).

--- command/commands.go
+++ command/commands.go
@@ -1,33 +1,39 @@
 package command
 
 import (
+	"os"
+
 	"github.com/mitchellh/cli"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func Commands(commandUI cli.Ui) map[string]cli.CommandFactory {
 
-	coloredUI := &cli.ColoredUi{
-		OutputColor: cli.UiColorNone,
-		InfoColor:   cli.UiColorBlue,
-		ErrorColor:  cli.UiColorRed,
-		WarnColor:   cli.UiColorYellow,
-		Ui:          commandUI,
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+
+		commandUI = &cli.ColoredUi{
+			OutputColor: cli.UiColorNone,
+			InfoColor:   cli.UiColorBlue,
+			ErrorColor:  cli.UiColorRed,
+			WarnColor:   cli.UiColorYellow,
+			Ui:          commandUI,
+		}
 	}
 
 	all := map[string]cli.CommandFactory{
 		"generate": func() (cli.Command, error) {
 			return &GenerateCommand{
-				Ui: coloredUI,
+				Ui: commandUI,
 			}, nil
 		},
 		"generate list": func() (cli.Command, error) {
 			return &GenerateListCommand{
-				Ui: coloredUI,
+				Ui: commandUI,
 			}, nil
 		},
 		"transfer": func() (cli.Command, error) {
 			return &TransferCommand{
-				Ui: coloredUI,
+				Ui: commandUI,
 			}, nil
 		},
 	}
