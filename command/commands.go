a7a94b5e73eeb673aa788e2203fe44a777031c33
James Woodruff <james.woodruff4@gmail.com>
2018-12-09 20:53:54 -0500

    Implemented images list feature.

--- command/commands.go
+++ command/commands.go
@@ -4,6 +4,10 @@ import (
 	"github.com/mitchellh/cli"
 )
 
+const (
+	API_URL = "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
+)
+
 func Commands(commandUI cli.Ui) map[string]cli.CommandFactory {
 
 	all := map[string]cli.CommandFactory{
@@ -14,6 +18,11 @@ func Commands(commandUI cli.Ui) map[string]cli.CommandFactory {
 		},
 		"generate list": func() (cli.Command, error) {
 			return &GenerateListCommand{
+				Ui: commandUI,
+			}, nil
+		},
+		"generate list-images": func() (cli.Command, error) {
+			return &GenerateListImagesCommand{
 				Ui: commandUI,
 			}, nil
 		},
